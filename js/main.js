9c51d0285a49340652d6a5e3e4951f8652d47f0b
uk090483 <konradullrich@me.com>
2020-04-23 10:32:05 +0200

    adding 404

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var imgaegWrap = document.querySelector('.images');
-var index = 0;
-setInterval(() => {
-  index = (index + 1) % 4;
-  imgaegWrap.dataset.image = index;
-}, 5000);
+// var imgaegWrap = document.querySelector('.images');
+// var index = 0;
+// setInterval(() => {
+//   index = (index + 1) % 4;
+//   imgaegWrap.dataset.image = index;
+// }, 5000);
