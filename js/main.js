14be37eedbd4bef72809dc691e19588d1cc3dfdc
uk090483 <konradullrich@me.com>
2020-04-30 17:23:17 +0200

    cleaning up

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// var imgaegWrap = document.querySelector('.images');
-// var index = 0;
-// setInterval(() => {
-//   index = (index + 1) % 4;
-//   imgaegWrap.dataset.image = index;
-// }, 5000);
